88def67b162434b98fa7c383ca5d06ba2ae1a6e6
Rubén <rubenaguadoc@gmail.com>
2018-08-05 09:16:21 +0200

    Updated SW registration and comments

--- src/sw/sw.js
+++ src/sw/sw.js
@@ -2,7 +2,7 @@ workbox.core.setCacheNameDetails({prefix: "my-app"});
 workbox.skipWaiting();
 workbox.clientsClaim();
 
-// Custom SW
+// Custom SW, to see some templates, auto generate them trough the webpack config runtimecching: [{...}]
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
